3447f10c2f7dea423d066cb385e7587908989c44
javaoneholmachine <training+javaoneholmachine@github.com>
2013-09-23 17:53:44 -0700

    Printed a message

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -10,6 +10,6 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("This is the practicing branch!");
     }
 }
