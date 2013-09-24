5da9b4114fedce6826e7426f8fec284290257036
javaoneholmachine <training+javaoneholmachine@github.com>
2013-09-23 17:33:24 -0700

    Printed a message

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,15 @@
+package javaapplication2;
+
+/**
+ *
+ * @author Lab
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
